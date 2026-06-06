59e675a47a4586bfde733fd873a6ca5717da89d7
gloomcowboy <phillshaw8004321@gmail.com>
2026-06-06 13:27:28 +0100

    better unbiased  char picking code

--- generator.js
+++ generator.js
@@ -1,14 +1,23 @@
 const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()+-_><|~^"
 
 function generatePassword(length = 12) {
-    const array = new Uint8Array(12);
-    crypto.getRandomValues(array)
-    let password = ""
-    for (let i = 0; i < length; i++) {
-        const randomIndex = array[i] % chars.length
-        password += chars[randomIndex]
+
+    const password = []
+    // we have 83 chars, 83 x 3 = 249, every char has a equal chance of getting picked, anything above that would be unfair and come chars would get picked more often, bias.
+    const limit = Math.floor(256 / chars.length) * chars.length; // 249
+
+    while (password.length < length) {
+        const array = new Uint8Array(length * 2) // oversample, so good unbiased chars get picked
+        crypto.getRandomValues(array)
+
+        for (const byte of array) {
+            if (password.length >= length) break
+            if (byte < limit) {
+                password.push(chars[byte % chars.length])
+            }
+        }
     }
-    return password
+    return password.join("")
 }
 
 function handleOutput(){
